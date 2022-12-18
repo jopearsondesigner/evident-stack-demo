01338c743d426abb3488481d0f30735275f832d4
jopearsondesigner <jopearsondesigner@gmail.com>
2022-12-18 16:02:42 -0700

    WIP factoring components: Footer.

---
+++ components/src/lib/footer/Footer.svelte
@@ -0,0 +1,12 @@
+<script lang='ts'>
+	export let footerClass:string = '';
+	export {footerClass as class};
+</script>
+
+<footer class={footerClass}>
+	<slot/>
+</footer>
+
+
+
+
---
+++ components/src/lib/footer/FooterLink.svelte
@@ -0,0 +1,13 @@
+<script lang='ts'>
+    export let liClass:string = 'w-full py-2';
+    export let aClass:string = 'text-sm text-brand-gray-1 hover:text-gray-brand-4 dark:text-gray-brand-4 dark:hover:text-gray-brand-1 transition duration-200 ease-in';
+    export {aClass as class};
+    export let href:string = '/';
+    export let label:string = '';
+</script>
+
+<li class={liClass}>
+    <a {href} class={aClass}>
+        {label}
+    </a>
+</li>

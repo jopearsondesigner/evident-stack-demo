23947e794b003e54867d3a9cc8c4796698c39fec
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2022-12-24 12:54:03 -0700

    WIP reorganizing project

---
+++ domain/schemas/src/lib.rs
@@ -0,0 +1,20 @@
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+mod cue;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}

cf020cb2af30fec39e643210c66126452a562f5d
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2022-12-30 00:56:50 -0700

    Split default tests module into separate file

--- domain/event-models/src/application/mod.rs
+++ domain/event-models/src/application/mod.rs
@@ -2,19 +2,19 @@ use std::collections::HashSet;
 use crate::{EventModel, EventModelId};
 // TODO: implement in terms of DTOs, as appropriate
 
-pub trait EventModelRepository {
-    type Error;
-
-    fn all_ids(&self) -> HashSet<EventModelId>;
-    fn by_id(&self, id: &EventModelId) -> Option<&dyn EventModel>;
-    fn by_name(&self, id: &str) -> Option<&dyn EventModel>;
-
-    fn put_by_id(
-        &self,
-        id: &EventModelId,
-        event_model: &dyn EventModel
-    ) -> Result<(), Self::Error>;
-}
+// pub trait EventModelRepository {
+//     type Error;
+//
+//     fn all_ids(&self) -> HashSet<EventModelId>;
+//     fn by_id(&self, id: &EventModelId) -> Option<& EventModel>;
+//     fn by_name(&self, id: &str) -> Option<&dyn EventModel>;
+//
+//     fn put_by_id(
+//         &self,
+//         id: &EventModelId,
+//         event_model: &dyn EventModel
+//     ) -> Result<(), Self::Error>;
+// }
 
 #[cfg(test)]
 mod tests {

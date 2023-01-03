7e84fa7d83532738dc3319dc98e82e23c26df498
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2023-01-03 14:26:07 -0700

    WIP hot-reloadable component + state + app build
    - Almost wired up state management into app

--- design/editor/app/src/routes/+page.svelte
+++ design/editor/app/src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+ import state from "$lib/state";
+
+ const handleClick = function(e) { state.greet() };
+</script>
+
 <h1>Welcome</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button on:click={handleClick}>Greet</button>

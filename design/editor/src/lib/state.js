bdd3c0e797eef70925e3c1342835149efc5b1370
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2023-01-10 22:58:20 -0700

    Basic command dispatch and model context loading

--- design/editor/src/lib/state.js
+++ design/editor/src/lib/state.js
@@ -1,3 +1,25 @@
-import init, { greet } from "editor-state";
+import { EventModelStateManager, EventModelCreationContext } from "editor-state";
+import { readable, derived } from 'svelte/store';
 
-export { greet };
+const initializeEventModelCreationContext = function() {
+  let creationContext = new EventModelCreationContext();
+  return {
+    dispatch: (command) => { creationContext.dispatch(command) }
+  };
+}
+
+const initializeEventModelStore = function(id) {
+  let manager = new EventModelStateManager(id);
+  let store = readable(null, set => {
+    manager.initialize(set);
+
+    return () => console.debug("Unsubscribed last subscriber to Event Model", id);
+  });
+
+  return {
+    store,
+    dispatch: (command) => { manager.dispatch(command) }
+  };
+}
+
+export { initializeEventModelStore, initializeEventModelCreationContext };
--- design/editor/state/src/lib.rs
+++ design/editor/state/src/lib.rs
@@ -1,6 +1,12 @@
+extern crate event_models;
+
 mod utils;
 
+use js_sys::Function;
 use wasm_bindgen::prelude::*;
+use event_models::domain::commands::EventModelCommand;
+use event_models::{EventModel, EventModelId};
+use event_models::default::DefaultEventModel;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -11,9 +17,59 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[wasm_bindgen]
 extern {
     fn alert(s: &str);
+
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, editor-state!");
+pub struct EventModelCreationContext;
+
+#[wasm_bindgen]
+impl EventModelCreationContext {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> EventModelCreationContext {
+        EventModelCreationContext
+    }
+
+    pub fn dispatch(&self, js_command: JsValue) -> Result<(), JsValue> {
+        let command: EventModelCommand = serde_wasm_bindgen::from_value(js_command)?;
+        log(&format!("Dispatching {:?}", command));
+        Ok(())
+    }
+}
+
+#[wasm_bindgen]
+pub struct EventModelStateManager {
+    id: EventModelId,
+    setter: Option<Function>
+}
+
+#[wasm_bindgen]
+impl EventModelStateManager {
+    #[wasm_bindgen(constructor)]
+    pub fn new(js_id: JsValue) -> Result<EventModelStateManager, JsValue> {
+        let id: EventModelId = serde_wasm_bindgen::from_value(js_id)?;
+        Ok(EventModelStateManager {
+            id,
+            setter: None
+        })
+    }
+
+    pub fn initialize(&mut self, setter: Function) {
+        self.setter = Some(setter);
+        match &self.setter {
+            None => (),
+            Some(setter) => {
+                setter.call1(&JsValue::null(), &JsValue::from(1));
+            }
+        };
+    }
+
+    pub fn dispatch(&self, js_command: JsValue) -> Result<(), JsValue> {
+        let command: EventModelCommand = serde_wasm_bindgen::from_value(js_command)?;
+        log(&format!("Dispatching {:?}", command));
+        log(&format!("Setter: {:?}", &self.setter));
+        Ok(())
+    }
 }

187e7f6f11c58dd28b62608caa717565b0b4d0c1
jopearsondesigner <jopearsondesigner@gmail.com>
2023-01-16 17:27:01 -0700

    Add use case page to fix broken link.

--- components/src/lib/typography/SectionCopy.svelte
+++ components/src/lib/typography/SectionCopy.svelte
@@ -3,7 +3,6 @@
 	export let pClass: string = 'text-body-light dark:text-body-dark leading-normal';
 	export let textSize: string = 'text-base';
 	export let classes: string = '';
-	// export {classes as class};
 </script>
 
 <p {...$$restProps} class={classNames(textSize, pClass, classes, $$props.class)}>

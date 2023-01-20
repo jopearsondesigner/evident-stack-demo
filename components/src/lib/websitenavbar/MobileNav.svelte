05834f4cd9434b9c562de4b82ddc7ca352061937
jopearsondesigner <jopearsondesigner@gmail.com>
2023-01-19 23:42:44 -0700

    Remove working files.

--- components/src/lib/websitenavbar/MobileNav.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	export let mobileNav: string = 'z-50';
-</script>
-
-<nav class={mobileNav}>
-	<slot />
-</nav>
--- components/src/lib/websitenavbar/NavToolbar.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	export let toolbarClass = 'px-3 h-9 border-x border-gray-secondary flex items-center';
-</script>
-
-<div class={toolbarClass}>
-	<slot />
-</div>

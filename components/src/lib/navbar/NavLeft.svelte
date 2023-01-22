5f178c2230cb50715158285d896633019a50495f
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2023-01-21 17:08:36 -0700

    Fix capitalization

---
+++ components/src/lib/navbar/NavLeft.svelte
@@ -0,0 +1,9 @@
+<script>
+	import classNames from 'classnames';
+	import './navbar.css';
+	export let navClass = 'inline-flex space-x-3 mx-3 items-center';
+</script>
+
+<nav class={classNames(navClass)}>
+	<slot />
+</nav>

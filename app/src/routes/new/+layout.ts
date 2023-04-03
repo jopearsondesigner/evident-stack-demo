f09a68ec844700eff292d569d775d577ebd2de1f
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2023-04-03 11:22:54 -0600

    One more type issue

--- app/src/routes/new/+layout.ts
+++ app/src/routes/new/+layout.ts
@@ -4,7 +4,7 @@ import type { LayoutLoad } from './$types';
 
 export const load: LayoutLoad = async (_event) => {
   if (browser) {
-    let {create_model} = await initializeEventModelStore(null);
+    let {create_model} = await initializeEventModelStore(undefined);
     return {create_model};
   }
 };

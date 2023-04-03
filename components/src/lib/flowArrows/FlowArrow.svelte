df6c3b6ede474a4114723eeca8657aa5e200792d
Mike Shearer <shearerbeard@gmail.com>
2023-04-03 17:09:41 -0600

    decided to split this out for svelte render - OG code is at components/src/lib/flowArrows/FlowArrow.js

---
+++ components/src/lib/flowArrows/FlowArrow.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let marker: string;
+    export let debugOverlay: string;
+    export let pathCoords: string;
+</script>
+<svg width="100%" height="100%" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
+    <defs>{ marker }</defs>
+    { debugOverlay } { pathCoords }
+</svg>

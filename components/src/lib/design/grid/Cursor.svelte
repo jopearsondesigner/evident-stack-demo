0779d398ad10f8fe0c7a3b7f48371e4aaf310fbf
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2023-04-04 14:48:54 -0600

    Scroll cursor into view when navigating

--- components/src/lib/design/grid/Cursor.svelte
+++ components/src/lib/design/grid/Cursor.svelte
@@ -2,11 +2,18 @@
   export let row: number;
   export let column: number;
 
+  let element: HTMLDivElement;
+
   $: gridRow = row + 1;
 	$: gridColumn = column + 1;
+
+  $: if (element && gridRow > 0 && gridColumn > 0) {
+    element.scrollIntoView({behavior: "smooth", block: "center", inline: "center"})
+  }
 </script>
 
 <div
+  bind:this={element}
 	class="cursor z-10 self-stretch w-full h-full transition duration-200 ease-in border-2 border-cyan-300"
 	style="grid-row: {gridRow} / {gridRow}; grid-column: {gridColumn} / {gridColumn};"
 />

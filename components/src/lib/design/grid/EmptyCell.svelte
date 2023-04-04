fb42e88fb939e2136776e922d02faa2b82044baf
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2023-04-04 16:06:21 -0600

    Whitespace

--- components/src/lib/design/grid/EmptyCell.svelte
+++ components/src/lib/design/grid/EmptyCell.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	export let row: number;
-	export let column: number;
+  export let row: number;
+  export let column: number;
   import { createEventDispatcher } from 'svelte';
   import type { MouseEventHandler } from 'svelte/elements';
 
-	$: gridRow = row + 1;
-	$: gridColumn = column + 1;
+  $: gridRow = row + 1;
+  $: gridColumn = column + 1;
 
   const dispatch = createEventDispatcher();
   const handleClick: MouseEventHandler<HTMLDivElement> = (_event) => {
@@ -15,6 +15,5 @@
 
 <div
   on:click|preventDefault|stopPropagation={handleClick}
-	class="empty-cell z-10 self-stretch relative min-w-placementPadded min-h-placementPadded dark:border-gray-brand-1 mb-px hover:bg-focus/[.18] transition duration-200 ease-in"
-	style="grid-row: {gridRow} / {gridRow}; grid-column: {gridColumn} / {gridColumn};"
-/>
+  class="empty-cell z-10 self-stretch relative min-w-placementPadded min-h-placementPadded dark:border-gray-brand-1 mb-px hover:bg-focus/[.18] transition duration-200 ease-in"
+  style="grid-row: {gridRow} / {gridRow}; grid-column: {gridColumn} / {gridColumn};" />

8e39a5d9b66a24588f5411205bace0c7551c84ac
Mike Shearer <shearerbeard@gmail.com>
2023-04-03 19:59:21 -0600

    add more component logic and update loop to root FlowArrow.svelte

--- components/src/lib/flowArrows/FlowArrow.svelte
+++ components/src/lib/flowArrows/FlowArrow.svelte
@@ -1,9 +1,63 @@
 <script lang="ts">
-    export let marker: string;
-    export let debugOverlay: string;
-    export let pathCoords: string;
+	import { onDestroy } from 'svelte';
+  import FlowMarker from './FlowMarker.svelte';
+	import type { Id, PathProps } from './types';
+
+	export let config: PathProps;
+
+	const rectForId = (id: Id): DOMRect | undefined => {
+		const element = document.getElementById(id);
+		if (element) {
+			return element.getBoundingClientRect();
+		}
+	};
+
+	const rectsUpdated = (current: DOMRect, next: DOMRect): boolean => {
+		return (
+			current.x !== next.x ||
+			current.y !== next.y ||
+			current.width !== next.width ||
+			current.height !== next.height
+		);
+	};
+
+	let toRect: DOMRect | undefined = rectForId(config.to);
+	let fromRect: DOMRect | undefined = rectForId(config.from);
+
+	let updateLoop = setInterval(() => {
+		const nextTo = rectForId(config.to);
+		const nextFrom = rectForId(config.from);
+
+        if (!nextTo || !nextFrom) {
+            return;
+        }
+
+        if (!toRect || !fromRect) {
+            toRect = nextTo;
+            fromRect = nextFrom;
+            update();
+        } else if (rectsUpdated(toRect, nextTo) || rectsUpdated(fromRect, nextFrom)) {
+            toRect = nextTo;
+            fromRect = nextFrom;
+            update();
+        }
+	}, config.refreshTime);
+
+    let update = () => {
+        console.info(`UPDATE: TO: ${config.to}, FROM: ${config.from}`)
+    }
+
+	onDestroy(() => {
+		clearInterval(updateLoop);
+	});
 </script>
+
 <svg width="100%" height="100%" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
+	<defs>
+        <FlowMarker color={config.color} size={config.markerSize} />
+    </defs>
+</svg>
+<!-- <svg width="100%" height="100%" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
     <defs>{ marker }</defs>
     { debugOverlay } { pathCoords }
-</svg>
+</svg> -->
--- components/src/lib/flowArrows/types.ts
+++ components/src/lib/flowArrows/types.ts
@@ -1,4 +1,38 @@
+export const RECT_ATTRIBUTES_TO_COMPARE = ['x', 'y', 'width', 'height'];
+
+export type Id = string;
+
 export interface Point {
     x: number;
     y: number;
+}
+
+export enum Anchor {
+    TOP,
+    LEFT,
+    BOTTOM,
+    RIGHT
+}
+
+export interface PathCoordinates {
+    to: Point;
+    from: Point;
+    mid: Point;
+    fromBezPoint: Point;
+    toBezPoint: Point;
+}
+
+export interface PathProps {
+    from: Id,
+    to: Id,
+    fromAnchor: Anchor,
+    toAnchor: Anchor,
+    color: string,
+    strokeWidth: number,
+    curveFactor: number,
+    offsetMarket: number,
+    markerSize: number,
+    dashness: boolean,
+    debug: boolean,
+    refreshTime?: number,
 }

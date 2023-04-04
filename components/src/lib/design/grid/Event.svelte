70c87eafbfcade544169b3e8c119b083a7e21905
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2023-04-04 16:07:36 -0600

    Whitespace

--- components/src/lib/design/grid/Event.svelte
+++ components/src/lib/design/grid/Event.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-	import MaybeTooltip from '../../utils/MaybeTooltip.svelte';
-	import markdown from '../../utils/markdown.js';
+  import MaybeTooltip from '../../utils/MaybeTooltip.svelte';
+  import markdown from '../../utils/markdown.js';
   import { createEventDispatcher } from 'svelte';
   import type { MouseEventHandler } from 'svelte/elements';
 
-	export let id: string;
-	export let event: string;
-	export let name: string;
-	export let description = '';
-	export let row: number;
-	export let column: number;
+  export let id: string;
+  export let event: string;
+  export let name: string;
+  export let description = '';
+  export let row: number;
+  export let column: number;
 
-	$: descriptionHTML = markdown(description);
-	$: gridRow = row + 1;
-	$: gridColumn = column + 1;
+  $: descriptionHTML = markdown(description);
+  $: gridRow = row + 1;
+  $: gridColumn = column + 1;
 
   const dispatch = createEventDispatcher();
   const handleClick: MouseEventHandler<HTMLDivElement> = (_event) => {
@@ -22,16 +22,16 @@
 </script>
 
 <div
-	{id}
+  {id}
   on:click|preventDefault|stopPropagation={handleClick}
-	class="placement event z-20 flex place-self-center align-items-center p-[1.375rem] dark:border-gray-brand-1 -ml-px mb-px hover:bg-focus/[.18] transition duration-200 ease-in"
-	style="grid-row: {gridRow} / {gridRow}; grid-column: {gridColumn} / {gridColumn};"
->
-	<MaybeTooltip tip={descriptionHTML}>
-		<div
-			class="event w-[6.125rem] h-[6.125rem] p-2 overflow-visible text-left text-node font-semibold leading-tight shadow-placement bg-gradient-to-b from-event-dark via-event to-event-light"
-		>
-			{name}
-		</div>
-	</MaybeTooltip>
+  class="placement event z-20 flex place-self-center align-items-center p-[1.375rem] dark:border-gray-brand-1 -ml-px mb-px hover:bg-focus/[.18] transition duration-200 ease-in"
+  style="grid-row: {gridRow} / {gridRow}; grid-column: {gridColumn} / {gridColumn};"
+  >
+  <MaybeTooltip tip={descriptionHTML}>
+    <div
+      class="event w-[6.125rem] h-[6.125rem] p-2 overflow-visible text-left text-node font-semibold leading-tight shadow-placement bg-gradient-to-b from-event-dark via-event to-event-light"
+      >
+      {name}
+    </div>
+  </MaybeTooltip>
 </div>
--- components/src/lib/design/grid/ReadModel.svelte
+++ components/src/lib/design/grid/ReadModel.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-	import MaybeTooltip from '../../utils/MaybeTooltip.svelte';
-	import markdown from '../../utils/markdown.js';
+  import MaybeTooltip from '../../utils/MaybeTooltip.svelte';
+  import markdown from '../../utils/markdown.js';
   import { createEventDispatcher } from 'svelte';
   import type { MouseEventHandler } from 'svelte/elements';
 
-	export let id: string;
-	export let readModel: string;
-	export let name: string;
-	export let description = '';
-	export let row: number;
-	export let column: number;
+  export let id: string;
+  export let readModel: string;
+  export let name: string;
+  export let description = '';
+  export let row: number;
+  export let column: number;
 
-	$: descriptionHTML = markdown(description);
-	$: gridRow = row + 1;
-	$: gridColumn = column + 1;
+  $: descriptionHTML = markdown(description);
+  $: gridRow = row + 1;
+  $: gridColumn = column + 1;
 
   const dispatch = createEventDispatcher();
   const handleClick: MouseEventHandler<HTMLDivElement> = (_event) => {
@@ -22,16 +22,16 @@
 </script>
 
 <div
-	{id}
+  {id}
   on:click|preventDefault|stopPropagation={handleClick}
-	class="placement read-model z-20 flex place-self-center align-items-center p-[1.375rem] dark:border-gray-brand-1 -ml-px mb-px hover:bg-focus/[.18] transition duration-200 ease-in"
-	style="grid-row: {gridRow} / {gridRow}; grid-column: {gridColumn} / {gridColumn};"
->
-	<MaybeTooltip tip={descriptionHTML}>
-		<div
-			class="readModel w-[6.125rem] h-[6.125rem] p-3 overflow-visible text-left text-node font-semibold leading-tight shadow-placement bg-gradient-to-b from-readModel to-readModel-light"
-		>
-			{name}
-		</div>
-	</MaybeTooltip>
+  class="placement read-model z-20 flex place-self-center align-items-center p-[1.375rem] dark:border-gray-brand-1 -ml-px mb-px hover:bg-focus/[.18] transition duration-200 ease-in"
+  style="grid-row: {gridRow} / {gridRow}; grid-column: {gridColumn} / {gridColumn};"
+  >
+  <MaybeTooltip tip={descriptionHTML}>
+    <div
+      class="readModel w-[6.125rem] h-[6.125rem] p-3 overflow-visible text-left text-node font-semibold leading-tight shadow-placement bg-gradient-to-b from-readModel to-readModel-light"
+      >
+      {name}
+    </div>
+  </MaybeTooltip>
 </div>

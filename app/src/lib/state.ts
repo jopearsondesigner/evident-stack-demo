10a1a2eca20bd8cf79744449054d4e57775e1ddb
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2023-04-06 18:04:03 -0600

    Fix some typescript errors

--- app/src/lib/state.ts
+++ app/src/lib/state.ts
@@ -18,25 +18,25 @@ const initialize_decider = async (id: string | undefined) => {
         return await manager.create(name)
       },
       define_and_place_interface: async (name: string, index: number, audience: string | undefined) => {
-        return await manager.define_and_place_interface(id, name, index, audience);
+        return await manager.define_and_place_interface(id!, name, index, audience);
       },
       define_and_place_command: async (name: string, index: number) => {
-        return await manager.define_and_place_command(id, name, index);
+        return await manager.define_and_place_command(id!, name, index);
       },
       define_and_place_event: async (name: string, index: number, stream: string | undefined) => {
-        return await manager.define_and_place_event(id, name, index, stream);
+        return await manager.define_and_place_event(id!, name, index, stream);
       },
       define_and_place_read_model: async (name: string, index: number) => {
-        return await manager.define_and_place_read_model(id, name, index);
+        return await manager.define_and_place_read_model(id!, name, index);
       },
       delete_model: async () => {
-        return await manager.delete(id)
+        return await manager.delete(id!)
       },
       import_json: async (json_bytes: Uint8Array, offset: number) => {
-        return await manager.import(id, json_bytes, offset)
+        return await manager.import(id!, json_bytes, offset)
       },
       rename_placement: async (placement: string, name: string) => {
-        return await manager.rename_placement(id, placement, name)
+        return await manager.rename_placement(id!, placement, name)
       },
     }
   };

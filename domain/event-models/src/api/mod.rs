8c1b1c6867f273123b5b7cbc3b9eb33c3a020994
Mike Shearer <shearerbeard@gmail.com>
2023-04-13 12:26:46 -0600

    implementation and additional validation function for lane manipulations

--- domain/event-models/src/api/mod.rs
+++ domain/event-models/src/api/mod.rs
@@ -10,7 +10,6 @@ use crate::types::{
 use crate::{EventModel, EventModelDataTransfer, EventModelState, ModifiableEventModel};
 use epoch::decider::{DeciderWithContext, Evolver};
 use std::fmt::Debug;
-use std::vec;
 use uuid::Uuid;
 
 pub mod commands;

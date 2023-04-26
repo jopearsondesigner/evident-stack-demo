38b45cb03fedf03afed604c8411280b0b9f43063
Mike Shearer <shearerbeard@gmail.com>
2023-04-25 18:25:54 -0600

    consolidate dragging lane state

--- components/src/lib/design/grid/util.ts
+++ components/src/lib/design/grid/util.ts
@@ -1,6 +1,7 @@
+import { Lane, PlacementType } from "../Grid";
+
 enum DraggingStateKind {
-    AUDIENCE,
-    STREAM,
+    LANE,
     INTERFACE,
     READ_MODEL,
     COMMAND,
@@ -9,12 +10,32 @@ enum DraggingStateKind {
 }
 
 type DraggingState =
-    | { kind: DraggingStateKind.AUDIENCE, value: { sourceId: string, targetIndex: number | undefined } }
-    | { kind: DraggingStateKind.STREAM, value: { sourceId: string, targetIndex: number | undefined } }
-    | { kind: DraggingStateKind.INTERFACE, value: { sourceId: string, target: { index: number, audienceId: string } | undefined } }
-    | { kind: DraggingStateKind.READ_MODEL, value: { sourceId: string, targetIndex: number | undefined } }
-    | { kind: DraggingStateKind.COMMAND, value: { sourceId: string, targetIndex: number | undefined } }
-    | { kind: DraggingStateKind.EVENT, value: { sourceId: string, target: { index: number, streamId: string } | undefined }} 
+    | { kind: DraggingStateKind.LANE,
+        value: {
+            source: {
+                laneId: string,
+                laneType: Lane,
+            },
+            target?: {
+                laneIndex: number,
+                laneType: Lane, 
+            }} }
+    | { kind: DraggingStateKind.INTERFACE,
+        value: {
+            source: { interfaceId: string },
+            target?: { column: number, audienceId: string } } }
+    | { kind: DraggingStateKind.READ_MODEL
+        value: {
+            source: { readModelId: string },
+            target?: { column: number } } }
+    | { kind: DraggingStateKind.COMMAND,
+        value: {
+            source: { commandId: string },
+            target?: { column: number }} }
+    | { kind: DraggingStateKind.EVENT,
+        value: {
+            source: { eventId: string },
+            target?: { column: number, streamId: string } }} 
     | { kind: DraggingStateKind.NONE };
 
 enum DraggingCommandKind {
@@ -29,28 +50,156 @@ enum DraggingCommandKind {
 }
 
 type DragCommand =
-    | { kind: DraggingCommandKind.LANE_DRAG_START, value: { laneId: string } }
-    | { kind: DraggingCommandKind.LANE_DRAG_ENTER, value: { index: number } }
-    | { kind: DraggingCommandKind.LANE_DRAG_LEAVE }
+    | { kind: DraggingCommandKind.LANE_DRAG_START,
+        value: {
+            laneId: string,
+            laneType: Lane,
+        } }
+    | { kind: DraggingCommandKind.LANE_DRAG_ENTER,
+        value: { laneIndex: number, laneType: Lane } }
+    | { kind: DraggingCommandKind.LANE_DRAG_LEAVE, }
     | { kind: DraggingCommandKind.LANE_DRAG_DROP }
-    | { kind: DraggingCommandKind.PLACEMENT_DRAG_START, value: { laneId: string } }
-    | { kind: DraggingCommandKind.PLACEMENT_DRAG_ENTER, value: { index: number } }
+    | { kind: DraggingCommandKind.PLACEMENT_DRAG_START,
+        value: {
+            placementId: string,
+            placementKind?: PlacementType
+        }}
+    | { kind: DraggingCommandKind.PLACEMENT_DRAG_ENTER,
+        value: {
+            column: number
+            laneIndex: number,
+            laneType: Lane,
+            laneId: string,
+        }}
     | { kind: DraggingCommandKind.PLACEMENT_DRAG_LEAVE }
     | { kind: DraggingCommandKind.PLACEMENT_DRAG_DROP }
 
 function decide(state: DraggingState, command: DragCommand): DraggingState {
-    switch(state.kind) {
-        case DraggingStateKind.AUDIENCE:
+    switch (command.kind) {
+        case DraggingCommandKind.LANE_DRAG_START: {
+            let { laneId, laneType } = command.value
+
+            return {
+                kind: DraggingStateKind.LANE,
+                value: {
+                    source: { laneId, laneType } ,
+                }
+            };
+        }
+            
+        case DraggingCommandKind.LANE_DRAG_ENTER: {
+            let { laneIndex, laneType } = command.value;
+
+            switch (state.kind) {
+                case DraggingStateKind.LANE:
+                    return {
+                        ...state,
+                        value: {
+                            ...state.value,
+                            target: {
+                                laneIndex,
+                                laneType
+                            }
+                            
+                        }
+                    };
+                default:
+                    return state;
+            }
+        } 
+        case DraggingCommandKind.LANE_DRAG_LEAVE:
+            switch (state.kind) {
+                case DraggingStateKind.LANE:
+                    return {
+                        ...state,
+                        value: {
+                            ...state.value,
+                            target: undefined
+                        }
+                    }
+                default:
+                    return state;
+            }
+        case DraggingCommandKind.LANE_DRAG_DROP:
             return state;
-        case DraggingStateKind.STREAM:
+        case DraggingCommandKind.PLACEMENT_DRAG_START:
             return state;
-        case DraggingStateKind.INTERFACE:
-            return state;
-        case DraggingStateKind.COMMAND:
-            return state;
-        case DraggingStateKind.EVENT:
-            return state;
-        default:
+        case DraggingCommandKind.PLACEMENT_DRAG_ENTER: {
+            switch (state.kind) {
+                default:
+                    return state;
+            }
+            // case DraggingStateKind.LANE:
+            // case DraggingStateKind.INTERFACE:
+            // case DraggingStateKind.READ_MODEL:
+            // case DraggingStateKind.COMMAND:
+            // case DraggingStateKind.EVENT:
+        }
+        case DraggingCommandKind.PLACEMENT_DRAG_LEAVE:
+            switch (state.kind) {
+                case DraggingStateKind.LANE: {
+                    return {
+                        ...state,
+                        value: {
+                            ...state.value,
+                            target: undefined
+                        }
+                    }
+                }
+                case DraggingStateKind.INTERFACE: {
+                    return {
+                        ...state,
+                        value: {
+                            ...state.value,
+                            target: undefined
+                        }
+                    }
+                }
+                case DraggingStateKind.READ_MODEL: {
+                    return {
+                        ...state,
+                        value: {
+                            ...state.value,
+                            target: undefined
+                        }
+                    }
+                }
+                case DraggingStateKind.COMMAND: {
+                    return {
+                        ...state,
+                        value: {
+                            ...state.value,
+                            target: undefined
+                        }
+                    }
+                }
+                case DraggingStateKind.EVENT: {
+                    return {
+                        ...state,
+                        value: {
+                            ...state.value,
+                            target: undefined
+                        }
+                    }
+                }
+                case DraggingStateKind.NONE:
+            }
+        case DraggingCommandKind.PLACEMENT_DRAG_DROP:
             return state;
     }
+
+    // switch(state.kind) {
+    //     case DraggingStateKind.AUDIENCE:
+    //         return state;
+    //     case DraggingStateKind.STREAM:
+    //         return state;
+    //     case DraggingStateKind.INTERFACE:
+    //         return state;
+    //     case DraggingStateKind.COMMAND:
+    //         return state;
+    //     case DraggingStateKind.EVENT:
+    //         return state;
+    //     default:
+    //         return state;
+    // }
 }

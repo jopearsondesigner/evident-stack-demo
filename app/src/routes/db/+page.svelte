646bad692b3cb79b8f51a67ee1d18f6b2970ea17
jopearsondesigner <jopearsondesigner@gmail.com>
2023-05-09 13:34:34 -0600

    Layer animations.

---
+++ app/src/routes/db/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Player from '$components/utils/Player.svelte';
+  import Button from '$components/Button.svelte';
+</script>
+
+<div class="h-screen flex justify-center items-center">
+  <div>
+    <Player src="lottie/DatabaseAnimation.json" width="500" />
+    <div class="flex justify-center mt-8">
+      <Button
+        input
+        boundTo="layers-form"
+        tabindex={0}
+        gradient
+        color="brandStackPrimary"
+        size="md"
+        label="Keep me updated"
+        class="m-3"
+      />
+    </div>
+  </div>
+</div>

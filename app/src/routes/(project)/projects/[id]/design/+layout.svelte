5ca9bb23c64dcd3c08c9bb54b4b81d00b6e11dfe
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2023-05-20 15:02:08 -0600

    Wire up keystrokes for placement details and import JSON navigations

--- app/src/routes/(project)/projects/[id]/design/+layout.svelte
+++ app/src/routes/(project)/projects/[id]/design/+layout.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
   import { goto } from '$app/navigation';
+  import { page } from '$app/stores';
   import Grid from '$components/design/Grid.svelte';
   import type { LayoutData } from './$types';
+  import { debug } from '$lib/util';
 
   export let data: LayoutData;
 
   const { grid, decider } = data;
+
+  const handleNavigateToPlacementDetails = (e: CustomEvent) => {
+    debug("handling navigateToPlacementDetails", e, `/projects/${$page.params.id}/design/placements/${e.detail.placement}`);
+    goto(`/projects/${$page.params.id}/design/placements/${e.detail.placement}`, {noScroll: true})
+  }
+
+  const handleNavigateToImportJson = (e: CustomEvent) => {
+    debug("handling navigateToPlacementDetails", e, `/projects/${$page.params.id}/design/placements/import?column=${e.detail.column}`);
+    goto(`/projects/${$page.params.id}/design/placements/import?column=${e.detail.column}`, {noScroll: true})
+  }
 </script>
 
 <slot />
@@ -18,4 +30,7 @@
   streams={$grid?.streams}
   default_stream_placements={$grid?.default_stream}
   flows={$grid?.flows}
-  column_count={$grid?.column_count || 0} />
+  column_count={$grid?.column_count || 0}
+  on:navigateToPlacementDetails={handleNavigateToPlacementDetails}
+  on:navigateToImportJson={handleNavigateToImportJson}
+  />

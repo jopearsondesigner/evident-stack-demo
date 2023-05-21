f782162ce8b2fcda15c5ed45ed7925cdd3007ce5
Mike Shearer <shearerbeard@gmail.com>
2023-05-21 16:25:12 -0600

    remove debug

--- components/src/lib/design/Grid.svelte
+++ components/src/lib/design/Grid.svelte
@@ -394,7 +394,6 @@
 />
 
 <h3>{mode}</h3>
-<h3>{ JSON.stringify(dragState) }</h3>
 <div class="overflow-auto z-[0] relative h-full w-full bg-gray-canvas dark:bg-dark-1">
   <div
     bind:this={containerRef}

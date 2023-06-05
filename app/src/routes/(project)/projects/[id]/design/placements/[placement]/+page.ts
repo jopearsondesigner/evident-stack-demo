b403a637a1d5d97204bee9d062d74a90f01d7986
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2023-06-05 09:33:18 -0600

    Full-size interface preview in placement details screen

--- app/src/routes/(project)/projects/[id]/design/placements/[placement]/+page.ts
+++ app/src/routes/(project)/projects/[id]/design/placements/[placement]/+page.ts
@@ -16,9 +16,7 @@ export const load: PageLoad = async ({ params, parent }) => {
       let placement: Placement = await new Promise((resolve, reject) => {
         let unsubscribe = grid!.subscribe((g) => {
           if (g) {
-            console.log("grid from subscription:", g)
             let p = g.placement_by_id(params.placement);
-            console.log("placement from grid:", p)
             if (p) {
               resolve(p);
             } else {
@@ -35,7 +33,7 @@ export const load: PageLoad = async ({ params, parent }) => {
         handle_close,
       };
     } catch (e) {
-      console.error("caught error", e)
+      console.error("caught error while looking up placement", params.placement, e)
       throw error(404, `No placement found with id ${params.placement}`)
     }
   } else {
--- components/src/lib/FigmaEmbed.svelte
+++ components/src/lib/FigmaEmbed.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-  export let url: URL;
-  export let width: number | string;
-  export let height: number | string;
+  export let url: string | undefined;
+  export let width: number | string = "100%";
+  export let height: number | string = "100%";
 </script>
 
+{#if url}
 <iframe
   title="Evident Stack Figma Embed"
   style="border: 1px solid rgba(0, 0, 0, 0.1);"
@@ -11,3 +12,4 @@
   {height}
   src="https://www.figma.com/embed?embed_host=evidentstack.com&url={url}"
   allowfullscreen />
+{/if}

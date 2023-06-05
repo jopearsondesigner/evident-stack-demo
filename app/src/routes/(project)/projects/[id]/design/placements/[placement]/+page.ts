b000876cefb1875ff6c95bfeff49665ba6ed13ce
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2023-06-05 08:48:26 -0600

    Lookup placement in placement details +page.ts

--- app/src/routes/(project)/projects/[id]/design/placements/[placement]/+page.ts
+++ app/src/routes/(project)/projects/[id]/design/placements/[placement]/+page.ts
@@ -1,13 +1,44 @@
+import { browser } from "$app/environment";
 import { goto } from "$app/navigation";
+import type { Placement } from "$components/design/Grid";
+import { error } from "@sveltejs/kit";
 import type { PageLoad } from "./$types";
 
-export const load: PageLoad = async ({ params }) => {
+export const load: PageLoad = async ({ params, parent }) => {
   const handle_close = async () => {
     await goto(`/projects/${params.id}/design`, { noScroll: true })
   }
 
-  return {
-    placement_id: params.placement,
-    handle_close,
-  };
+  if (browser) {
+    const { grid } = await parent();
+
+    try {
+      let placement: Placement = await new Promise((resolve, reject) => {
+        let unsubscribe = grid!.subscribe((g) => {
+          if (g) {
+            console.log("grid from subscription:", g)
+            let p = g.placement_by_id(params.placement);
+            console.log("placement from grid:", p)
+            if (p) {
+              resolve(p);
+            } else {
+              reject("placement not found!")
+            }
+          } else {
+            reject("no grid found!")
+          }
+        });
+        unsubscribe();
+      });
+      return {
+        placement,
+        handle_close,
+      };
+    } catch (e) {
+      console.error("caught error", e)
+      throw error(404, `No placement found with id ${params.placement}`)
+    }
+  } else {
+    return { handle_close }
+  }
 };

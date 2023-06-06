f509f3fa9372cc3a092d691b36784177318efc1b
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2023-06-06 13:34:02 -0600

    Disable SSR for Dexie-dependent paths

--- app/src/routes/(non-project)/+page.ts
+++ app/src/routes/(non-project)/+page.ts
@@ -1,19 +1,15 @@
-import { browser } from "$app/environment";
 import { error } from "@sveltejs/kit";
 import type { PageLoad } from "./$types";
 
+// Can't use SSR with dexie-observable
+export const ssr = false;
+
 export const load: PageLoad = async (event) => {
   let { session } = await event.parent()
-  if (browser) {
+  if (session) {
     const { models_for_user } = await import("$lib/state/dexie");
-    if (session) {
-      const projects = await models_for_user(session.user.id);
-      return { projects }
-    }
-    throw error(404, "not found")
-  } else {
-    // TODO: map `_projects` from remote into Model[]
-    // const projects = await supabase.from("models").select();
-    return { projects:  []}
+    const projects = await models_for_user(session.user.id);
+    return { projects }
   }
+  throw error(404, "not found")
 }
--- app/src/routes/(project)/projects/[id]/+layout.ts
+++ app/src/routes/(project)/projects/[id]/+layout.ts
@@ -1,8 +1,10 @@
 import { initialize_decider } from '$lib/state/event_model';
-import { browser } from '$app/environment';
 import type { LayoutLoad } from './$types';
 import { error } from '@sveltejs/kit';
 import { model_by_id as remote_model_by_id } from '$lib/supabase/database';
+
+// Can't use SSR with dexie-observable
+export const ssr = false;
 
 export const load: LayoutLoad = async (event) => {
   let { session, supabase } = await event.parent()
@@ -10,21 +12,17 @@ export const load: LayoutLoad = async (event) => {
   const model_id = event.params.id;
 
   if (session) {
-    if (browser) {
-      const { model_by_id } = await import('$lib/state/dexie');
-      const local_model = await model_by_id(session.user.id, model_id);
-      if (local_model) {
+    const { model_by_id } = await import('$lib/state/dexie');
+    const local_model = await model_by_id(session.user.id, model_id);
+    if (local_model) {
+      let { grid, decider } = await initialize_decider(model_id, session.user.id);
+      return { grid, decider, session };
+    } else {
+      const remote_model = await remote_model_by_id(supabase, model_id);
+      if (remote_model) {
         let { grid, decider } = await initialize_decider(model_id, session.user.id);
         return { grid, decider, session };
-      } else {
-        const remote_model = await remote_model_by_id(supabase, model_id);
-        if (remote_model) {
-          let { grid, decider } = await initialize_decider(model_id, session.user.id);
-          return { grid, decider, session };
-        }
       }
-    } else {
-      return {}
     }
   }
   throw error(404, { message: "not found" });

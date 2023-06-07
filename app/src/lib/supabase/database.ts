b67622757de0b2a5565da5072178a091759833b4
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2023-06-06 22:31:41 -0600

    Properly handle error in supabase model_by_id

--- app/src/lib/supabase/database.ts
+++ app/src/lib/supabase/database.ts
@@ -4,7 +4,7 @@ import type { Database } from '$lib/supabase/database.types';
 export const models_for_user = async (supabase: SupabaseClient<Database>) => {
   let result = await supabase.from("models").select();
   if (result.error) {
-    throw result.error
+    throw result.error;
   } else {
     return result.data;
   }
@@ -12,5 +12,9 @@ export const models_for_user = async (supabase: SupabaseClient<Database>) => {
 
 export const model_by_id = async (supabase: SupabaseClient<Database>, id: string) => {
   let result = await supabase.from("models").select().eq('id', id).limit(1);
-  return result.data?.at(0);
+  if (result.error) {
+    throw result.error;
+  } else {
+    return result.data?.at(0);
+  }
 }

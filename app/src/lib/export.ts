c1ce78b6fb8a6f3feaec0e7244d9005862a2cd2a
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2023-06-15 01:15:37 -0600

    One more import/build issue

--- app/src/lib/export.ts
+++ app/src/lib/export.ts
@@ -1,6 +1,6 @@
-import { saveAs } from 'file-saver';
+import FileSaver from 'file-saver';
 
 export const exportJson = (filename: string, content: string) => {
   const blob = new Blob([content], { type: "application/json;charset=utf-8" });
-  saveAs(blob, `${filename}.json`, { autoBom: true });
+  FileSaver.saveAs(blob, `${filename}.json`, { autoBom: true });
 };

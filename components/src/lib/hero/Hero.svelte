54a6231d33b243c8cace741d923bfd8cb0fae3f7
jopearsondesigner <jopearsondesigner@gmail.com>
2023-07-28 18:25:35 -0600

    TS error fixes

--- components/src/lib/hero/Hero.svelte
+++ components/src/lib/hero/Hero.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   export let heroClass = 'h-full bg-white dark:bg-dark-1 text-body-light dark:text-body-dark';
-  export let style;
+  export let style = '';
 </script>
 
-<section class={heroClass}>
+<section class={heroClass} {style}>
   <slot />
 </section>

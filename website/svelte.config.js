5f6a7c71780f4b9540f4855cffa3d6b52c9e638f
jopearsondesigner <jopearsondesigner@gmail.com>
2026-05-23 19:27:49 -0600

    Switch Evident Stack to static Netlify build

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -1,19 +1,24 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
-		alias: {
-			"$components": '../components/src/lib/*'
-		}
-	},
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	]
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+      precompress: false
+    }),
+    alias: {
+      $components: '../components/src/lib/*'
+    }
+  },
+  preprocess: [
+    preprocess({
+      postcss: true
+    })
+  ]
 };
 
 export default config;
